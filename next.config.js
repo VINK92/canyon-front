43f92693acc48507b64f6e3b1a55a159613eb850
Viktoriia <nazaruk.v92@gmail.com>
2024-01-06 15:36:35 +0200

    setup localization

--- next.config.js
+++ next.config.js
@@ -4,6 +4,35 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack: (config, { dev }) => {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === "object")
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    // if (!dev) {
+    //   rules.forEach((rule) => {
+    //     rule.use.forEach((moduleLoader) => {
+    //       if (
+    //         moduleLoader.loader?.includes("css-loader") &&
+    //         !moduleLoader.loader?.includes("postcss-loader")
+    //       ) {
+    //         // eslint-disable-next-line no-param-reassign
+    //         moduleLoader.options.modules.getLocalIdent = hashOnlyIdent;
+    //       }
+    //     });
+    //   });
+    // }
+
+    if (dev) {
+      // eslint-disable-next-line no-param-reassign
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
